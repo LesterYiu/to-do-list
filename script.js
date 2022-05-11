68b1d80fda9af46dbc2b208d9ad25d299aa30c6f
Lester Yiu <lester.ws.yiu@gmail.com>
2022-05-11 16:28:17 -0400

    added js to take in user input with checkbox

--- script.js
+++ script.js
@@ -6,6 +6,8 @@
 
     only if the user has entere an actual task (AKA input is not submitted)
 
+    clear input field when submitted
+
     grab the user's to do item from the form input
     display to do on the page within an li element
         include a checkbox icon within the li
@@ -21,3 +23,59 @@
     Add a "Take a Break" message if 5 or more tasks are completed
 */
 
+// NOTE: element is typically refered to as "el" in variable names
+
+// NOTE: when tracking user input, it is conventional to use the query element's value however textLength/tetContent would work as well
+
+// NOTE: by using innerHTML and textContent on the same element, each by setting new content, will overwrite one another. Erasing the one stated before it. This overwritting behaviour is to either create a new element and appending it or utilizing text nodes
+
+
+
+
+
+
+
+const formEl = document.querySelector('form');
+
+const inputEl = document.getElementById('toDoItem');
+
+const ulEl = document.querySelector('ul');
+
+
+console.log(inputEl);
+
+formEl.addEventListener('submit', function (e) {
+    e.preventDefault();  
+            //Utilizing the event object, we'll be able to prevent the default behaviour of a form when it is submitted which is when it is submits, it will reload.
+    
+
+
+    if (inputEl.value !== '') {
+
+        console.log('A value has been entered!');
+
+        console.log(inputEl.value);
+
+        const liEl = document.createElement('li');
+
+        liEl.innerHTML = `<i class="fa-regular fa-square"></i>`;
+        
+        const toDoContent = document.createTextNode(`${inputEl.value}`);
+        //By creating text nodes, we get the added value of adding it to innerHTML without overwriting it
+
+        liEl.append(toDoContent);
+
+        console.log(toDoContent);
+
+        ulEl.appendChild(liEl);
+
+        
+
+        inputEl.value = '';
+        
+    } else {
+
+        alert('Please do not leave input empty before submitting the form.');
+
+    }
+});
