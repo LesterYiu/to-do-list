e54665efecad25b25b97d535638e5fa0d3ead242
Lester Yiu <lester.ws.yiu@gmail.com>
2022-05-11 14:40:07 -0400

    Added pseudo code comments for script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+/* PSEUDO CODE
+
+    add a submit event listener on the form
+
+    stop the page from refreshing when the form is submitted
+
+    only if the user has entere an actual task (AKA input is not submitted)
+
+    grab the user's to do item from the form input
+    display to do on the page within an li element
+        include a checkbox icon within the li
+    add the li element to the ul
+
+    clicking on a task allows you to toggle between checked/unchecked (AKA done vs not done) 
+
+    BONUS: 
+
+    Add a "reset" button which clears all the TO-DOs
+    Add a "remove" task button to each task
+    Add a congratulations alert when all of the existing TO DOs are checked off
+    Add a "Take a Break" message if 5 or more tasks are completed
+*/
+
